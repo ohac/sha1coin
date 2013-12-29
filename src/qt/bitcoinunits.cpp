dd60bedcc34ea797f84e74ac4492cc309aa0ae24
OHASHI Hideya <ohachige@gmail.com>
2013-12-29 18:40:11 +0900

    litecoin to sha1coin

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("SHA");
+    case mBTC: return QString("mSHA");
+    case uBTC: return QString::fromUtf8("μSHA");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Sha1coins");
+    case mBTC: return QString("Milli-Sha1coins (1 / 1,000)");
+    case uBTC: return QString("Micro-Sha1coins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
