808ab1ae1eaa77cd10ca5caa9e5168eb796d9a92
OHASHI Hideya <ohachige@gmail.com>
2013-12-29 12:04:40 +0900

    change address prefix L to S

--- src/base58.h
+++ src/base58.h
@@ -272,7 +272,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 48, // Litecoin addresses start with L
+        PUBKEY_ADDRESS = 64, // Sha1coin addresses start with S
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
