eff6b5550cd59e186a97bcd36cf5bb70d563a3dc
OHASHI Hideya <ohachige@gmail.com>
2014-03-15 23:39:26 +0900

    Add DNS seed (shaseed.sighash.info)

--- src/net.cpp
+++ src/net.cpp
@@ -1192,10 +1192,12 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
+    {"sighash.info", "shaseed.sighash.info"},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
+    {"sighash.info", "shaseed.sighash.info"}, // TODO
     {NULL, NULL}
 };
 
