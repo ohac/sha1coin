e22c89a0993125e283b6437828385725b702941a
kurema <kurema@users.noreply.github.com>
2017-12-21 17:17:19 +0900

    Update rpcblockchain.cpp

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -107,7 +107,7 @@ Value settxfee(const Array& params, bool fHelp)
     if (fHelp || params.size() < 1 || params.size() > 1)
         throw runtime_error(
             "settxfee <amount SHA/KB>\n"
-            "<amount> is a real and is rounded to the nearest 0.00000001 LTC per KB");
+            "<amount> is a real and is rounded to the nearest 0.00000001 SHA per KB");
 
     // Amount
     int64 nAmount = 0;
